3cd88e915bde8f750bb3c7b479bb2d72b8ee8971
Zarytskyi Oleksandr <pfantom180@gmail.com>
2026-05-26 16:46:52 +0300

    task is completed!

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,54 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+
+    private static final String SPECIFIED_CHARACTER = "w";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+
+        StringBuilder stringBuilder = new StringBuilder();
+        File file = new File(fileName);
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line).append(" ");
+
+            }
+            String string = stringBuilder.toString();
+            String[] m = string.split("\\W+");
+
+            int count = 0;
+            for (String element : m) {
+                String sequence = element.toLowerCase();
+                if (!sequence.isEmpty() && sequence.charAt(0) == 'w') {
+                    count++;
+                }
+            }
+
+            String[] result = new String[count];
+            int index = 0;
+
+            for (String element : m) {
+                String sequence = element.toLowerCase();
+                if (!sequence.isEmpty() && sequence.charAt(0) == 'w') {
+                    result[index] = sequence;
+                    index++;
+                }
+            }
+
+            Arrays.sort(result);
+            return result;
+
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file");
+        }
+
     }
+
 }
